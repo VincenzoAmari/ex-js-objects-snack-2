a4e34d9020a051d45e3d28e76025a6d36c09fd44
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-02 12:46:19 +0200

    es 06

--- main.js
+++ main.js
@@ -84,32 +84,63 @@
 
 // Code Question 5 (Bonus)
 
-const hamburger = {
-  name: "Cheese Burger",
-  weight: 250,
-  maker: {
-    name: "Anonymous Chef",
-    restaurant: {
-      name: "Hyur's Burgers",
-      address: "Main Street, 123",
-      isOpen: true,
-    },
-    age: 29,
-  },
-};
+// const hamburger = {
+//   name: "Cheese Burger",
+//   weight: 250,
+//   maker: {
+//     name: "Anonymous Chef",
+//     restaurant: {
+//       name: "Hyur's Burgers",
+//       address: "Main Street, 123",
+//       isOpen: true,
+//     },
+//     age: 29,
+//   },
+// };
 
-const newRestaurant = { ...hamburger.maker.restaurant };
-newRestaurant.name = "Hyur's II";
-newRestaurant.address = "Second Street, 12";
-const secondBurger = { ...hamburger };
-secondBurger.maker.restaurant = newRestaurant;
-secondBurger.maker.name = "Chef Hyur";
+// const newRestaurant = { ...hamburger.maker.restaurant };
+// newRestaurant.name = "Hyur's II";
+// newRestaurant.address = "Second Street, 12";
+// const secondBurger = { ...hamburger };
+// secondBurger.maker.restaurant = newRestaurant;
+// secondBurger.maker.name = "Chef Hyur";
 
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 
-console.log(hamburger.maker.name); // ? Chef Hyur
-console.log(secondBurger.maker.name); // ? Chef Hyur
-console.log(hamburger.maker.restaurant.name); // ? Hyur's II
-console.log(secondBurger.maker.restaurant.name); // ? Hyur's II
+// console.log(hamburger.maker.name); // ? Chef Hyur
+// console.log(secondBurger.maker.name); // ? Chef Hyur
+// console.log(hamburger.maker.restaurant.name); // ? Hyur's II
+// console.log(secondBurger.maker.restaurant.name); // ? Hyur's II
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 5
+
+//////////////////////////////////////////////////////////////////////////////////
+
+//Code Question 6 (Bonus)
+
+const chef = {
+  name: "Chef Hyur",
+  age: 29,
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+  restaurant: {
+    name: "Hyur's Burgers",
+    welcomeClient: () => {
+      console.log("Benvenuto!");
+    },
+    address: {
+      street: "Main Street",
+      number: 123,
+      showAddress: () => {
+        console.log("Main Street 123");
+      },
+    },
+    isOpen: true,
+  },
+};
+
+//Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+//allora questo è un caso particolare perchè con tutti i metodi perderemmo qualcosa, con quello superficiale (...chef) avemmo la funzione ma perderemmo gli annidamenti
+// con lo structureClone avremmo gli annidamenti ma perderemmo la funzione, se dobbiamo oprate per le 2 scelierei la seconda, meglio rifare solo la funzione
+// informandomi ho visto che cè un modo con una libreria  esterna chiamata lodash per fare un clone deep e avere sia funzioni che annidamenti quindi la scelta migliore
