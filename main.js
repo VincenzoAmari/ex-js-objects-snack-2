e0e2991e93a876090314bcb4fb26a0cfca413c70
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-02 10:18:07 +0200

    es 4

--- main.js
+++ main.js
@@ -4,20 +4,74 @@
 // secondBurger.name = "Double Cheese Burger";
 // secondBurger.weight = 500;
 
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+
 // console.log(hamburger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
 // console.log(secondBurger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
 
-// Code Question 2
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?   1
 
-const hamburger = {
-  name: "Cheese Burger",
-  weight: 250,
-  ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+//////////////////////////////////////////////////////////////////////////////////
+
+// Code Question 2
+// const hamburger = {
+//   name: "Cheese Burger",
+//   weight: 250,
+//   ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+// };
+
+// const secondBurger = { ...hamburger };
+// secondBurger.ingredients[0] = "Salad";
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// console.log(hamburger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
+// console.log(secondBurger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?   2
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Code Question 3
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?  3
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Code Question 4
+const chef = {
+  name: "Chef Hyur",
+  age: 29,
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
 };
 
-const secondBurger = { ...hamburger };
-secondBurger.ingredients[0] = "Salad";
+const restaurant = {
+  name: "Hyur's Burgers",
+  address: {
+    street: "Main Street",
+    number: 123,
+  },
+  openingDate: new Date(2025, 3, 11),
+  isOpen: false,
+};
 
-console.log(hamburger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché?   per questo utilizzerei lo spread perchè mi crea una copia e copia anche la funzione presente dentro l oggetto
 
-console.log(secondBurger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?  per questo utilizzerei uno structuredClone per via della presenza di oggetti complessi
