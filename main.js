113b3e05ecf11520f555251699f98588c7e48ab1
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-02 10:02:21 +0200

    sto facento tutto senza avviare il codice, se sono sbagliati li correggerò dopo aver visto la correzione

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const hamburger = { name: "Cheese Burger", weight: 250 };
-const secondBurger = hamburger;
-secondBurger.name = "Double Cheese Burger";
-secondBurger.weight = 500;
+// Code Question 1
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+// secondBurger.name = "Double Cheese Burger";
+// secondBurger.weight = 500;
 
-console.log(hamburger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
-console.log(secondBurger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
+// console.log(hamburger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
+// console.log(secondBurger.name); // ?  { name: "Double Cheese Burger", weight: 500 };
+
+// Code Question 2
+
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
+
+console.log(secondBurger.ingredients[0]); // ?{ name: "Cheese Burger", weight: 250,ingredients: ["Salad", "Meat", "Bread", "Tomato"]
